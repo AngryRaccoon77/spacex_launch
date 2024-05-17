f3729ebfb810d15dc6da34d7ffc987abfee4f334
AngryRaccoon <03sidcrafter@gmail.com>
2024-05-17 15:57:39 +0300

    refactor game

--- src/app/component/game/game.component.ts
+++ src/app/component/game/game.component.ts
@@ -1,6 +1,5 @@
-// game.component.ts
 import { Component, HostListener, OnInit } from '@angular/core';
-import {HeaderComponent} from "../header/header.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-game',
@@ -12,118 +11,167 @@ import {HeaderComponent} from "../header/header.component";
   standalone: true
 })
 export class GameComponent implements OnInit {
+  private SHIP_SPEED!: number;
+  private BULLET_SPEED!: number;
+  private ALIEN_BULLET_SPEED!: number;
+  private MYSTERY_SHIP_INTERVAL = 30000;
+
   canvas!: HTMLCanvasElement;
   ctx!: CanvasRenderingContext2D;
   shipX: number = 250;
   shipY: number = 700;
-  alienX: number = 30;
-  alienY: number = 30;
-  alienWidth: number = 32;
-  alienHeight: number = 32;
-  alienDX: number = 1;
-  alienDY: number = 0.1;
   bulletX!: number;
   bulletY!: number;
   bulletFired: boolean = false;
-  aliens: {x: number, y: number, dx: number, dy: number, points: number, image: HTMLImageElement}[] = [];
-  score: number = 0; // добавьте эту строку
-  lives: number = 3; // добавьте эту строку
-  alienBullets: {x: number, y: number}[] = [];
+  aliens: { x: number, y: number, dx: number, dy: number, points: number, image: HTMLImageElement }[] = [];
+  alienBullets: { x: number, y: number }[] = [];
   mysteryShipX: number = -100;
   mysteryShipY: number = 0;
-  mysteryShipDX: number = 2;
+  score: number = 0;
+  lives: number = 3;
   waveCount: number = 0;
-  mysteryShipAppearing: boolean = true;
+  mysteryShipAppearing: boolean = false;
+
   alienImage1: HTMLImageElement = new Image();
   alienImage2: HTMLImageElement = new Image();
   alienImage3: HTMLImageElement = new Image();
   shipImage: HTMLImageElement = new Image();
   mysteryShipImage: HTMLImageElement = new Image();
 
-
+  // Dynamic sizes
+  shipWidth: number = 8;
+  shipHeight: number = 64;
+  alienWidth: number = 8;
+  alienHeight: number = 32;
+  mysteryShipWidth: number = 72;
+  mysteryShipHeight: number = 72;
 
   ngOnInit() {
+    this.initializeCanvas();
+    this.initializeImages();
+    this.calculateSizes();
+    this.initializeSpeeds();
+    this.initializeAliens();
+    this.startGameLoops();
+  }
+
+  private initializeCanvas() {
     this.canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
     this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight-150;
-    const context = this.canvas.getContext('2d');
-    if (!context) {
-      throw new Error('Failed to get 2D context');
-    }
-    this.ctx = context;
-    setInterval(() => this.draw(), 10);
-    setInterval(() => this.alienShoot(), 2000);
-    setTimeout(() => this.appearMysteryShip(), 30000); // мистический корабль появится через 30 секунд
+    this.canvas.height = window.innerHeight - 150;
+    this.shipY = this.canvas.height - this.shipHeight;
+    this.ctx = this.canvas.getContext('2d')!;
+  }
+
+  private initializeImages() {
     this.alienImage1.src = 'assets/img/alien1.png';
     this.alienImage2.src = 'assets/img/alien2.png';
     this.alienImage3.src = 'assets/img/alien3.png';
     this.shipImage.src = 'assets/img/ship.png';
     this.mysteryShipImage.src = 'assets/img/roadster.png';
+
+    // Wait for images to load before calculating sizes
+    this.alienImage1.onload = () => this.calculateSizes();
+    this.alienImage2.onload = () => this.calculateSizes();
+    this.alienImage3.onload = () => this.calculateSizes();
+    this.shipImage.onload = () => this.calculateSizes();
+    this.mysteryShipImage.onload = () => this.calculateSizes();
+  }
+
+  private calculateSizes() {
+    const canvasWidth = this.canvas.width;
+    const canvasHeight = this.canvas.height;
+    this.shipWidth = canvasWidth * 0.03;
+    this.shipHeight = this.shipImage.height / this.shipImage.width * this.shipWidth;
+    this.alienWidth = canvasWidth * 0.02;
+    this.alienHeight = this.alienImage1.height / this.alienImage1.width * this.alienWidth;
+    this.mysteryShipWidth = canvasWidth * 0.05;
+    this.mysteryShipHeight = this.mysteryShipImage.height / this.mysteryShipImage.width * this.mysteryShipWidth;
+  }
+
+  private initializeSpeeds() {
+    this.SHIP_SPEED = this.canvas.width * 0.02;
+    this.BULLET_SPEED = this.canvas.height * 0.015;
+    this.ALIEN_BULLET_SPEED = this.canvas.height * 0.01;
+  }
+
+  private initializeAliens() {
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 5; j++) {
-        let points;
-        let image;
-        if (j == 0) {
-          points = 40;
-          image = this.alienImage3;
-
-        } else if (j < 3 && j >= 1) {
-          points = 20;
-          image = this.alienImage2;
-
-        } else {
-          points = 10;
-          image = this.alienImage1;
-        }
+        const { points, image } = this.getAlienAttributes(j);
         this.aliens.push({
-          x: 30 + i * 60,
-          y: 30 + j * 60,
-          dx: 0.1,
-          dy: 0.01,
+          x: this.canvas.width * 0.004 + i * this.canvas.width * 0.035,
+          y: this.canvas.height * 0.05 + j * this.canvas.height * 0.05,
+          dx: this.canvas.width * 0.00005,
+          dy: this.canvas.height * 0.00001,
           points: points,
           image: image
-      });
-    }
+        });
+      }
     }
   }
 
-  appearMysteryShip() {
+  private getAlienAttributes(row: number) {
+    let points, image;
+    if (row === 0) {
+      points = 40;
+      image = this.alienImage3;
+    } else if (row < 3) {
+      points = 20;
+      image = this.alienImage2;
+    } else {
+      points = 10;
+      image = this.alienImage1;
+    }
+    return { points, image };
+  }
+
+  private startGameLoops() {
+    setInterval(() => this.draw(), 10);
+    setInterval(() => this.alienShoot(), 2000);
+    setTimeout(() => this.appearMysteryShip(), this.MYSTERY_SHIP_INTERVAL);
+  }
+
+  private appearMysteryShip() {
     this.mysteryShipX = 0;
     this.mysteryShipAppearing = true;
-    setInterval(() => this.appearMysteryShip(), 30000);
-
+    setTimeout(() => this.appearMysteryShip(), this.MYSTERY_SHIP_INTERVAL);
   }
-  draw() {
+
+  private draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.drawShip();
-    this.drawAlien();
-    if (this.bulletFired) {
-      this.drawBullet();
-    }
-    this.drawAlienBullet();
-    if (this.mysteryShipAppearing) {
-      this.drawMysteryShip();
-    }
+    this.drawEntities();
+    this.checkCollisions();
     this.checkWin();
     this.checkLoss();
   }
 
-  drawShip() {
-
-    this.ctx.drawImage(this.shipImage, this.shipX, this.shipY, 64, 64);
-
+  private drawEntities() {
+    this.drawShip();
+    this.drawAliens();
+    if (this.bulletFired) {
+      this.drawBullet();
+    }
+    this.drawAlienBullets();
+    if (this.mysteryShipAppearing) {
+      this.drawMysteryShip();
+    }
   }
 
-  drawMysteryShip() {
-    this.ctx.drawImage(this.mysteryShipImage, this.mysteryShipX, this.mysteryShipY, 72, 72);
-    this.mysteryShipX += this.mysteryShipDX;
+  private drawShip() {
+    this.ctx.drawImage(this.shipImage, this.shipX, this.shipY, this.shipWidth, this.shipHeight);
+  }
+
+  private drawMysteryShip() {
+    this.ctx.drawImage(this.mysteryShipImage, this.mysteryShipX, this.mysteryShipY, this.mysteryShipWidth, this.mysteryShipHeight);
+    this.mysteryShipX += this.canvas.width * 0.0015;
     if (this.mysteryShipX > this.canvas.width) {
       this.mysteryShipAppearing = false;
     }
   }
 
-  drawAlien() {
-    this.aliens.forEach((alien, ) => {
+  private drawAliens() {
+    this.aliens.forEach(alien => {
       this.ctx.drawImage(alien.image, alien.x, alien.y, this.alienWidth, this.alienHeight);
       alien.x += alien.dx;
       alien.y += alien.dy;
@@ -133,108 +181,103 @@ export class GameComponent implements OnInit {
       if (alien.y + this.alienHeight > this.canvas.height || alien.y < 0) {
         alien.dy = -alien.dy;
       }
-
     });
   }
-  drawBullet() {
+
+  private drawBullet() {
     this.ctx.fillStyle = '#FFFF00';
-    this.ctx.fillRect(this.bulletX, this.bulletY, 5, 10);
-    this.bulletY -= 10;
+    this.ctx.fillRect(this.bulletX, this.bulletY, this.canvas.width * 0.002, this.canvas.height * 0.025);
+    this.bulletY -= this.BULLET_SPEED;
     if (this.bulletY < 0) {
       this.bulletFired = false;
     }
-    this.aliens.forEach((alien, index) => {
-      if (this.bulletX < alien.x + this.alienWidth &&
-        this.bulletX + 5 > alien.x &&
-        this.bulletY < alien.y + this.alienHeight &&
-        this.bulletY + 10 > alien.y) {
-        // Bullet hit the alien
-        this.aliens.splice(index, 1);
-        this.bulletFired = false;
-        this.score += alien.points; // увеличиваем счет
-        this.aliens.forEach(alien => { // увеличиваем скорость пришельцев
-          alien.dx *= 1.1;
-          alien.dy *= 1.05;
-        });
-      }
-    });
-    if (this.bulletX < this.mysteryShipX + 50 &&
-      this.bulletX + 5 > this.mysteryShipX &&
-      this.bulletY < this.mysteryShipY + 20 &&
-      this.bulletY + 10 > this.mysteryShipY) {
-      // Bullet hit the mystery ship
-      this.mysteryShipAppearing = false;
-      this.score += 100; // увеличиваем счет
-    }
   }
-  alienShoot() {
-    if (this.aliens.length > 0) {
-      const randomAlien = this.aliens[Math.floor(Math.random() * this.aliens.length)];
-      this.alienBullets.push({x: randomAlien.x + this.alienWidth / 2, y: randomAlien.y + this.alienHeight});
-    }
-  }
-  drawAlienBullet() {
+
+  private drawAlienBullets() {
     this.alienBullets.forEach((bullet, index) => {
       this.ctx.fillStyle = '#0000FF';
-      this.ctx.fillRect(bullet.x, bullet.y, 5, 10);
-      bullet.y += 5;
+      this.ctx.fillRect(bullet.x, bullet.y, this.canvas.width * 0.002, this.canvas.height * 0.025);
+      bullet.y += this.ALIEN_BULLET_SPEED;
       if (bullet.y > this.canvas.height) {
         this.alienBullets.splice(index, 1);
-      }
-      if (bullet.x < this.shipX + 50 &&
-        bullet.x + 5 > this.shipX &&
-        bullet.y < this.shipY + 20 &&
-        bullet.y + 10 > this.shipY) {
-        // Bullet hit the ship
-        this.alienBullets.splice(index, 1);
-        this.lives -= 1; // уменьшаем количество жизней
       }
     });
   }
 
-  checkWin() {
+  private checkCollisions() {
+    this.checkBulletCollisions();
+    this.checkAlienBulletCollisions();
+  }
+
+  private checkBulletCollisions() {
+    this.aliens.forEach((alien, index) => {
+      if (this.bulletFired && this.isCollision(this.bulletX, this.bulletY, 5, 10, alien.x, alien.y, this.alienWidth, this.alienHeight)) {
+        this.aliens.splice(index, 1);
+        this.bulletFired = false;
+        this.score += alien.points;
+        this.increaseAlienSpeed();
+      }
+    });
+    if (this.bulletFired && this.isCollision(this.bulletX, this.bulletY, 5, 10, this.mysteryShipX, this.mysteryShipY, this.mysteryShipWidth, this.mysteryShipHeight)) {
+      this.mysteryShipAppearing = false;
+      this.mysteryShipAppearing = false;
+      const mysteryShipScores = [50, 100, 150, 200, 300];
+      const randomScore = mysteryShipScores[Math.floor(Math.random() * mysteryShipScores.length)];
+      this.score += randomScore;
+    }
+  }
+
+  private isCollision(x1: number, y1: number, w1: number, h1: number, x2: number, y2: number, w2: number, h2: number) {
+    return x1 < x2 + w2 && x1 + w1 > x2 && y1 < y2 + h2 && y1 + h1 > y2;
+  }
+
+  private increaseAlienSpeed() {
+    this.aliens.forEach(alien => {
+      alien.dx *= 1.07;
+      alien.dy *= 1.02;
+    });
+  }
+
+  private checkAlienBulletCollisions() {
+    this.alienBullets.forEach((bullet, index) => {
+      if (this.isCollision(bullet.x, bullet.y, 5, 10, this.shipX, this.shipY, this.shipWidth, this.shipHeight)) {
+        this.alienBullets.splice(index, 1);
+        this.lives -= 1;
+      }
+    });
+  }
+
+  private alienShoot() {
+    if (this.aliens.length > 0) {
+      const randomAlien = this.aliens[Math.floor(Math.random() * this.aliens.length)];
+      this.alienBullets.push({ x: randomAlien.x + this.alienWidth / 2, y: randomAlien.y + this.alienHeight });
+    }
+  }
+
+  private checkWin() {
     if (this.aliens.length === 0) {
       alert('Новая волна!');
       this.createAlienWave();
     }
   }
 
-  checkLoss() {
-    if (this.lives <= 0) { // проверяем количество жизней
+  private checkLoss() {
+    if (this.lives <= 0 || this.aliens.some(alien => alien.y >= this.shipY)) {
       alert('Вы проиграли!');
       this.restartGame();
-    } else {
-      for (let i = 0; i < this.aliens.length; i++) {
-        if (this.aliens[i].y >= this.shipY) {
-          alert('Вы проиграли!');
-          this.restartGame();
-          break;
-        }
-      }
     }
   }
 
-  createAlienWave() {
+  private createAlienWave() {
     this.waveCount++;
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 5; j++) {
-        let points;
-        let image;
-        if (j < 2) {
-          points = 10;
-          image= this.alienImage1;
-        } else if (j === 2) {
-          points = 20;
-          image = this.alienImage2;
-        } else {
-          points = 40;
-          image = this.alienImage3;
-        }
+        const { points, image } = this.getAlienAttributes(j);
         this.aliens.push({
-          x: 30 + i * 60,
-          y: 30 + j * 60,
-          dx: 0.1 * 1.05 * this.waveCount,
-          dy: 0.01 * 1.01 * this.waveCount,
+          x: this.canvas.width * 0.004 + i * this.canvas.width * 0.035,
+          y: this.canvas.height * 0.05 + j * this.canvas.height * 0.05,
+          dx: this.canvas.width * 0.00005 * 1.0005 * this.waveCount,
+          dy: this.canvas.height * 0.00001 * 1.000001 * this.waveCount,
           points: points,
           image: image
         });
@@ -242,58 +285,57 @@ export class GameComponent implements OnInit {
     }
   }
 
-  restartGame() {
+  private restartGame() {
     this.shipX = 250;
-    this.shipY = 850;
-    this.alienX = 30;
-    this.alienY = 30;
-    this.alienDX = 1;
-    this.alienDY = 0.1;
+    this.shipY = this.canvas.height - this.shipHeight - 20;
     this.bulletFired = false;
     this.aliens = [];
     this.score = 0;
-
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 5; j++) {
-        let points;
-        let image;
-        if (j < 2) {
-          points = 10;
-          image= this.alienImage1;
-        } else if (j === 2) {
-          points = 20;
-          image = this.alienImage2;
-        } else {
-          points = 40;
-          image = this.alienImage3;
-        }
-        this.aliens.push({
-          x: 30 + i * 60,
-          y: 30 + j * 60,
-          dx: 0.1,
-          dy: 0.01,
-          points: points,
-          image: image
-        });
-      }
-    }
     this.lives = 3;
     this.alienBullets = [];
+    this.initializeAliens();
+  }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight - 150;
+    this.calculateSizes();
+    this.initializeSpeeds();
   }
 
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
-    if (event.key === 'ArrowLeft' && this.shipX > 0) {
-      this.shipX -= 15;
-    } else if (event.key === 'ArrowRight' && this.shipX < this.canvas.width - 50) {
-      this.shipX += 15;
-    } else if (event.key === ' ') {
-      if (!this.bulletFired) {
-        this.bulletX = this.shipX + 22.5;
-        this.bulletY = this.shipY - 10;
-        this.bulletFired = true;
-      }
+    switch (event.key) {
+      case 'ArrowLeft':
+        this.moveShipLeft();
+        break;
+      case 'ArrowRight':
+        this.moveShipRight();
+        break;
+      case ' ':
+        this.fireBullet();
+        break;
+    }
+  }
+
+  private moveShipLeft() {
+    if (this.shipX > 0) {
+      this.shipX -= this.SHIP_SPEED;
+    }
+  }
+
+  private moveShipRight() {
+    if (this.shipX < this.canvas.width - this.shipWidth) {
+      this.shipX += this.SHIP_SPEED;
+    }
+  }
+
+  private fireBullet() {
+    if (!this.bulletFired) {
+      this.bulletX = this.shipX + this.shipWidth / 2 - 2.5;
+      this.bulletY = this.shipY - 10;
+      this.bulletFired = true;
     }
   }
 }
